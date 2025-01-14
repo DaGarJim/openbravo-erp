273e5bc95ffde5753e68317751a6d7db544eefae
Atul Gaware <atul.gaware@openbravo.com>
2025-01-14 15:25:27 +0530

    Related to 0057627: Error in Box-UnBox transaction when processing with User having limited Organization access
    **Update copyright year

--- src/org/openbravo/materialmgmt/refinventory/ReferencedInventoryProcessor.java
+++ src/org/openbravo/materialmgmt/refinventory/ReferencedInventoryProcessor.java
@@ -11,7 +11,7 @@
  * under the License. 
  * The Original Code is Openbravo ERP. 
  * The Initial Developer of the Original Code is Openbravo SLU 
- * All portions are Copyright (C) 2017-2024 Openbravo SLU 
+ * All portions are Copyright (C) 2017-2025 Openbravo SLU
  * All Rights Reserved. 
  * Contributor(s):  ______________________________________.
  ************************************************************************
